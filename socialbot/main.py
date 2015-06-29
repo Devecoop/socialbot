4ede686c6723ab95340fed3031abe4cae5856d5e
Juan Manuel Schillaci <jmschillaci@gmail.com>
2015-06-28 22:34:22 -0400

    Added Relayer + twitterer

--- socialbot/main.py
+++ socialbot/main.py
@@ -8,40 +8,36 @@ from socialbot.utils import get_config
 config = get_config()
 MAGIC_KEYWORD = config.get('main', 'magic_keyword')
 
-# Add wiki directory just for now
-import sys
-sys.path.append("/srv/deploys/moinmoin/moin-1.9.7/")
-
 
 class SlackBotHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_len = int(self.headers.getheader('content-length', 0))
         post_body = self.rfile.read(content_len)
 
-        import pdb;pdb.set_trace()
         try:
             postvars = cgi.parse_qs(post_body, keep_blank_values=1)
             user_name = postvars.get('user_name')[0]
-            text = postvars.get('text')[0]
-            text = text.strip("{} <".format(MAGIC_KEYWORD)).rstrip(">")
-
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-
+            content = postvars.get('text')[0]
+            raw_link = content.strip("{} <".format(MAGIC_KEYWORD)).rstrip(">")
+            link = raw_link.split("|")[0]
+            text = content.split(">")[1]
             # TODO: Change this, so it can return an status for each of
             # the actions
-            map(lambda x:x.do(text), self.plugin_list)
+            map(lambda x:x.do(text, link), self.plugin_list)
 
             actions = [k.ACTION_NAME for k in self.plugin_list]
             action_string = " and ".join(actions)
 
             payload = '{"text" : "Thanks for the link, %s. It has been %s "}' % (user_name, action_string)
         except Exception as err:
-            payload = '{"text" : "Sorry %s! Could not process link}' % user_name
+            payload = '{"text" : "Sorry %s! Could not process link"}' % user_name
+        finally:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            # Send the html message
+            self.wfile.write(payload)
 
-        # Send the html message
-        self.wfile.write(payload)
         return
 
 try:
@@ -51,10 +47,9 @@ try:
     PORT_NUMBER = int(config.get('main', 'port'))
     # TODO: Read this from config file and import them accordingly
     from socialbot.plugins.twitterer import Twitterer
-    from socialbot.plugins.moiner import Moiner
-    #plugin_list = [Twitterer(), Moiner()]
+    from socialbot.plugins.relayer import Relayer
     #plugin_list = [Twitterer()]
-    plugin_list = [Moiner()]
+    plugin_list = [Relayer(), Twitterer()]
 
     SlackBotHandler.plugin_list = plugin_list
     server = HTTPServer(('', PORT_NUMBER), SlackBotHandler)
