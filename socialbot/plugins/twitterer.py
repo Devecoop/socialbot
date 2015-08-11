d9a8cea41bbed060ecf2e90c79d060776585fdc2
Juan Manuel Schillaci <jmschillaci@gmail.com>
2015-08-11 18:25:59 -0300

    Added documentation for setting up twitter account

--- socialbot/plugins/twitterer.py
+++ socialbot/plugins/twitterer.py
@@ -1,5 +1,13 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
+# Steps to generate your token/secret credentials
+# 1) After installing the requirements execute the command twitter on the cli
+# 2) This will open the browser and will ask you to accept the authorization
+# 3) This will create a PIN for you, you should input this in the console.
+# 4) A file called .twitter_oauth will be written on your home directory, first
+# line is the token, second the secret
+# 5) Copy secret and token and paste them uto the appropiate section on
+# socialbot.ini file
+
 #from pyshorteners.shorteners  import Shortener
 from twitter import *
 
@@ -7,6 +15,8 @@ from socialbot.utils import get_config
 
 TWITTER_CONSUMER_KEY = 'uS6hO2sV6tDKIOeVjhnFnQ'
 TWITTER_CONSUMER_SECRET = 'MEYTOS97VvlHX7K1rwHPEqVpTSqZ71HtvoK4sVuYk'
+
+
 
 
 class Twitterer(object):
